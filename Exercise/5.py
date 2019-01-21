f59cada22a1f200b2e1285bc32f36838a5891dbb
iamnotamacbook <lty974322131>
2019-01-21 17:26:31 +0800

    2019.1.21

---
+++ Exercise/5.py
@@ -0,0 +1,31 @@
+a = 1
+b = 1
+
+if( a == b ):
+    print("当a=", a, "b=", b, "时,a等于b。")
+else:
+    print("当a=", a, "b=", b, "时,a不等于b。")
+
+a = 2
+b = 1
+
+if( a != b ):
+    print("当a=", a, "b=", b, "时,a不等于b。")
+else:
+    print("当a=", a, "b=", b, "时,a等于b。")
+
+a = 1
+b = 2
+
+if( a > b ):
+    print("当a=", a, "b=", b, "时,a大于b。")
+else:
+    print("当a=", a, "b=", b, "时,a小于b。")
+
+a = 2
+b = 1
+
+if( a < b ):
+    print("当a=", a, "b=", b, "时,a小于b。")
+else:
+    print("当a=", a, "b=", b, "时,a大于b。")
