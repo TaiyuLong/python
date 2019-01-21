d84af1dc4ce77cf00ef7b137f26139e0752278d2
iamnotamacbook <lty974322131>
2019-01-21 17:08:50 +0800

    2019.1.21

---
+++ Exercise/4.py
@@ -0,0 +1,37 @@
+a = 21
+b = 10
+c = 0
+
+print("当a=", a, "b=", b, "时")
+
+c = a + b
+print("a加b的值为：", c)
+
+c = a - b
+print("a减b的值为：", c)
+
+c = a * b
+print("a*b的值为：", c)
+
+c = a / b
+print("a除以b的值为：", c)
+
+c = a % b
+print("a取整的值为：", c)
+
+# 修改变量 a 、b 、c
+a = 2
+b = 3
+
+print("当a=", a, "b=", b, "时")
+
+c = a ** b
+print("a的b次方的值为：", c)
+
+a = 10
+b = 5
+
+print("当a=", a, "b=", b, "时")
+
+c = a // b
+print("a整除b的值为：", c)
